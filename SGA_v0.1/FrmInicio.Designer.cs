53a0be382696f0ec22c5cafb4c1db9c31e7ad78a
Ramon C.S <cedillojuanramon1d@gmail.com>
2025-10-31 21:39:11 -0600

    3. Se preparó FrmInicio y se agregaron recursos.

--- SGA_v0.1/FrmInicio.Designer.cs
+++ SGA_v0.1/FrmInicio.Designer.cs
@@ -28,20 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tsPrincipal = new System.Windows.Forms.ToolStrip();
+            this.tsbInicio = new System.Windows.Forms.ToolStripButton();
+            this.tsbProveedores = new System.Windows.Forms.ToolStripButton();
+            this.tsbCategoria = new System.Windows.Forms.ToolStripButton();
+            this.tsbNotificacion = new System.Windows.Forms.ToolStripButton();
+            this.tsbProductos = new System.Windows.Forms.ToolStripButton();
+            this.tsbEntradas = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalidas = new System.Windows.Forms.ToolStripButton();
+            this.tsbReportes = new System.Windows.Forms.ToolStripButton();
+            this.tsbRolesPermisos = new System.Windows.Forms.ToolStripButton();
+            this.tsbUsuarios = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalir = new System.Windows.Forms.ToolStripButton();
+            this.tsPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tsPrincipal
+            // 
+            this.tsPrincipal.AutoSize = false;
+            this.tsPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tsPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbInicio,
+            this.tsbProveedores,
+            this.tsbCategoria,
+            this.tsbNotificacion,
+            this.tsbProductos,
+            this.tsbEntradas,
+            this.tsbSalidas,
+            this.tsbReportes,
+            this.tsbRolesPermisos,
+            this.tsbUsuarios,
+            this.tsbSalir});
+            this.tsPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tsPrincipal.Name = "tsPrincipal";
+            this.tsPrincipal.Size = new System.Drawing.Size(60, 755);
+            this.tsPrincipal.TabIndex = 1;
+            this.tsPrincipal.Text = "toolStrip1";
+            // 
+            // tsbInicio
+            // 
+            this.tsbInicio.AutoSize = false;
+            this.tsbInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tsbInicio.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbInicio.Image = global::SGA_v0._1.Properties.Resources.Inicio;
+            this.tsbInicio.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbInicio.Name = "tsbInicio";
+            this.tsbInicio.Size = new System.Drawing.Size(60, 60);
+            this.tsbInicio.Text = "INICIO";
+            // 
+            // tsbProveedores
+            // 
+            this.tsbProveedores.AutoSize = false;
+            this.tsbProveedores.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbProveedores.Image = global::SGA_v0._1.Properties.Resources.Proveedores;
+            this.tsbProveedores.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbProveedores.Name = "tsbProveedores";
+            this.tsbProveedores.Size = new System.Drawing.Size(60, 60);
+            this.tsbProveedores.Text = "PROVEEDORES";
+            // 
+            // tsbCategoria
+            // 
+            this.tsbCategoria.AutoSize = false;
+            this.tsbCategoria.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCategoria.Image = global::SGA_v0._1.Properties.Resources.Categoria;
+            this.tsbCategoria.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCategoria.Name = "tsbCategoria";
+            this.tsbCategoria.Size = new System.Drawing.Size(60, 60);
+            this.tsbCategoria.Text = "CATEGORIA";
+            // 
+            // tsbNotificacion
+            // 
+            this.tsbNotificacion.AutoSize = false;
+            this.tsbNotificacion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNotificacion.Image = global::SGA_v0._1.Properties.Resources.Notificacion;
+            this.tsbNotificacion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNotificacion.Name = "tsbNotificacion";
+            this.tsbNotificacion.Size = new System.Drawing.Size(60, 60);
+            this.tsbNotificacion.Text = "NOTIFICACION";
+            // 
+            // tsbProductos
+            // 
+            this.tsbProductos.AutoSize = false;
+            this.tsbProductos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbProductos.Image = global::SGA_v0._1.Properties.Resources.Producto;
+            this.tsbProductos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbProductos.Name = "tsbProductos";
+            this.tsbProductos.Size = new System.Drawing.Size(60, 60);
+            this.tsbProductos.Text = "PRODUCTOS";
+            // 
+            // tsbEntradas
+            // 
+            this.tsbEntradas.AutoSize = false;
+            this.tsbEntradas.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEntradas.Image = global::SGA_v0._1.Properties.Resources.Registro_Entrada;
+            this.tsbEntradas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEntradas.Name = "tsbEntradas";
+            this.tsbEntradas.Size = new System.Drawing.Size(60, 60);
+            this.tsbEntradas.Text = "ENTRADAS";
+            // 
+            // tsbSalidas
+            // 
+            this.tsbSalidas.AutoSize = false;
+            this.tsbSalidas.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalidas.Image = global::SGA_v0._1.Properties.Resources.Registro_Salida;
+            this.tsbSalidas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalidas.Name = "tsbSalidas";
+            this.tsbSalidas.Size = new System.Drawing.Size(60, 60);
+            this.tsbSalidas.Text = "SALIDAS";
+            // 
+            // tsbReportes
+            // 
+            this.tsbReportes.AutoSize = false;
+            this.tsbReportes.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbReportes.Image = global::SGA_v0._1.Properties.Resources.Reporte;
+            this.tsbReportes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbReportes.Name = "tsbReportes";
+            this.tsbReportes.Size = new System.Drawing.Size(60, 60);
+            this.tsbReportes.Text = "REPORTES";
+            // 
+            // tsbRolesPermisos
+            // 
+            this.tsbRolesPermisos.AutoSize = false;
+            this.tsbRolesPermisos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbRolesPermisos.Image = global::SGA_v0._1.Properties.Resources.Rol_Permisos;
+            this.tsbRolesPermisos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRolesPermisos.Name = "tsbRolesPermisos";
+            this.tsbRolesPermisos.Size = new System.Drawing.Size(60, 60);
+            this.tsbRolesPermisos.Text = "ROLES Y PERMISOS";
+            // 
+            // tsbUsuarios
+            // 
+            this.tsbUsuarios.AutoSize = false;
+            this.tsbUsuarios.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbUsuarios.Image = global::SGA_v0._1.Properties.Resources.Usuarios;
+            this.tsbUsuarios.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbUsuarios.Name = "tsbUsuarios";
+            this.tsbUsuarios.Size = new System.Drawing.Size(60, 60);
+            this.tsbUsuarios.Text = "USUARIOS";
+            // 
+            // tsbSalir
+            // 
+            this.tsbSalir.AutoSize = false;
+            this.tsbSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalir.Image = global::SGA_v0._1.Properties.Resources.Salir;
+            this.tsbSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalir.Name = "tsbSalir";
+            this.tsbSalir.Size = new System.Drawing.Size(60, 60);
+            this.tsbSalir.Text = "SALIR";
             // 
             // FrmInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 755);
+            this.Controls.Add(this.tsPrincipal);
+            this.IsMdiContainer = true;
             this.Name = "FrmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmInicio";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tsPrincipal.ResumeLayout(false);
+            this.tsPrincipal.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip tsPrincipal;
+        private System.Windows.Forms.ToolStripButton tsbInicio;
+        private System.Windows.Forms.ToolStripButton tsbProveedores;
+        private System.Windows.Forms.ToolStripButton tsbCategoria;
+        private System.Windows.Forms.ToolStripButton tsbNotificacion;
+        private System.Windows.Forms.ToolStripButton tsbProductos;
+        private System.Windows.Forms.ToolStripButton tsbEntradas;
+        private System.Windows.Forms.ToolStripButton tsbSalidas;
+        private System.Windows.Forms.ToolStripButton tsbReportes;
+        private System.Windows.Forms.ToolStripButton tsbRolesPermisos;
+        private System.Windows.Forms.ToolStripButton tsbUsuarios;
+        private System.Windows.Forms.ToolStripButton tsbSalir;
     }
 }
 
